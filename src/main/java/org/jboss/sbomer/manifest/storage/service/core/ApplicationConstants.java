dda2829b2d321ea3757e3744a62081ed47271ffe
Mert Bicak <mbicak@redhat.com>
2025-11-20 14:14:32 +0100

    feat: initial project and folder structure

---
+++ src/main/java/org/jboss/sbomer/manifest/storage/service/core/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package org.jboss.sbomer.manifest.storage.service.core;
+
+public class ApplicationConstants {
+
+    private ApplicationConstants() {}
+
+    public static final String COMPONENT_NAME = "manifest-storage-service";
+
+}
